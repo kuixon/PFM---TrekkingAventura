54696e426c5d35e17d759bc879c2ca339ef3d4d2
kuixon <salgueiro93@gmail.com>
2017-02-20 13:26:29 +0100

    Se añade la utilización de la API del tiempo en la vista en detalle de cada excursión.

--- TrekkingAventura/app/src/main/java/es/deusto/trekkingaventura/fragments/ExcursionFragment.java
+++ TrekkingAventura/app/src/main/java/es/deusto/trekkingaventura/fragments/ExcursionFragment.java
@@ -1,15 +1,25 @@
 package es.deusto.trekkingaventura.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.json.JSONException;
 
 import es.deusto.trekkingaventura.R;
 import es.deusto.trekkingaventura.entities.Excursion;
+import es.deusto.trekkingaventura.entities.Weather;
+import es.deusto.trekkingaventura.utilities.JSONWeatherParser;
+import es.deusto.trekkingaventura.utilities.WeatherHttpClient;
 
 /**
  * Created by salgu on 15/02/2017.
@@ -28,6 +38,18 @@ public class ExcursionFragment extends Fragment {
     private ImageView imgLevel;
     private TextView txtLatitude;
     private TextView txtLongitude;
+
+    // Parámetros situación meteorológica
+    private TextView cityText;
+    private TextView condDescr;
+    private TextView temp;
+    private TextView press;
+    private TextView windSpeed;
+    private TextView windDeg;
+    private TextView hum;
+    private ImageView imgView;
+    private LinearLayout panelTiempo;
+    private LinearLayout panelTiempoNoDisponible;
 
     public ExcursionFragment() {
 
@@ -56,6 +78,18 @@ public class ExcursionFragment extends Fragment {
         imgLevel = (ImageView) rootView.findViewById(R.id.excLevel);
         txtLatitude = (TextView) rootView.findViewById(R.id.excLatitude);
         txtLongitude = (TextView) rootView.findViewById(R.id.excLongitude);
+
+        // Parámetros meteorológicos
+        cityText = (TextView) rootView.findViewById(R.id.cityText);
+        condDescr = (TextView) rootView.findViewById(R.id.condDescr);
+        temp = (TextView) rootView.findViewById(R.id.temp);
+        hum = (TextView) rootView.findViewById(R.id.hum);
+        press = (TextView) rootView.findViewById(R.id.press);
+        windSpeed = (TextView) rootView.findViewById(R.id.windSpeed);
+        windDeg = (TextView) rootView.findViewById(R.id.windDeg);
+        imgView = (ImageView) rootView.findViewById(R.id.condIcon);
+        panelTiempo = (LinearLayout) rootView.findViewById(R.id.panelTiempo);
+        panelTiempoNoDisponible = (LinearLayout) rootView.findViewById(R.id.panelTiempoNoDisponible);
 
         switch (excursion.getImgPath()) {
             case "Cares":
@@ -90,6 +124,61 @@ public class ExcursionFragment extends Fragment {
         txtLatitude.setText(Float.toString(excursion.getLatitude()));
         txtLongitude.setText(Float.toString(excursion.getLongitude()));
 
+        // Se realiza la petición a la API del tiempo.
+        String city = excursion.getLocation();
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city});
+
         return rootView;
+    }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            if (data != null) {
+                try {
+                    weather = JSONWeatherParser.getWeather(data);
+
+                    // Let's retrieve the icon
+                    weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.i("Error", "la API no ha devuelto ningun valor");
+            }
+
+            return weather;
+
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather != null) {
+                if (weather.iconData != null && weather.iconData.length > 0) {
+                    Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+                    imgView.setImageBitmap(img);
+                }
+
+                cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
+                condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+                temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "ºC");
+                hum.setText("" + weather.currentCondition.getHumidity() + "%");
+                press.setText("" + weather.currentCondition.getPressure() + " hPa");
+                windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+                windDeg.setText("" + weather.wind.getDeg() + "º");
+
+                panelTiempo.setVisibility(View.VISIBLE);
+            } else {
+                panelTiempoNoDisponible.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
---
+++ TrekkingAventura/app/src/main/java/es/deusto/trekkingaventura/utilities/WeatherHttpClient.java
@@ -0,0 +1,84 @@
+package es.deusto.trekkingaventura.utilities;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by salgu on 20/02/2017.
+ */
+
+public class WeatherHttpClient {
+
+    private static String API_KEY = "c62dd1a67db43c63b19e933e51028163";
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String IMG_URL = "http://openweathermap.org/img/w/";
+
+    public String getWeatherData(String location) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(BASE_URL + location + "&APPID=" + API_KEY)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            is.close();
+            con.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+    }
+
+    public byte[] getImage(String code) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+        try {
+            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            is = con.getInputStream();
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            while ( is.read(buffer) != -1)
+                baos.write(buffer);
+
+            return baos.toByteArray();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+    }
+}
