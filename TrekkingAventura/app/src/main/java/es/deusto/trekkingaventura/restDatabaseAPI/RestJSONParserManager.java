a9225afd959bc3fc998a28fa1e805caa048eeb02
kuixon <salgueiro93@gmail.com>
2017-03-22 12:00:21 +0100

    Se corrige el parseador de JSON porque no se contemplaba la posibilidad de que nos devolviera en algunos casos un JSONObject en lugar de un JSONArray.

--- TrekkingAventura/app/src/main/java/es/deusto/trekkingaventura/restDatabaseAPI/RestJSONParserManager.java
+++ TrekkingAventura/app/src/main/java/es/deusto/trekkingaventura/restDatabaseAPI/RestJSONParserManager.java
@@ -3,6 +3,7 @@ package es.deusto.trekkingaventura.restDatabaseAPI;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 
@@ -49,18 +50,14 @@ public class RestJSONParserManager {
         ArrayList<OpinionExtendida> loe = new ArrayList<OpinionExtendida>();
 
         JSONObject root = new JSONObject(data);
-        JSONArray opinionesExtendidas = root.getJSONArray("opinionExtendida");
 
-        OpinionExtendida oe;
-        JSONObject opinionExtendida;
-        JSONObject excursion;
-        JSONObject usuario;
-        for (int i=0; i < opinionesExtendidas.length(); i++) {
-            oe = new OpinionExtendida();
+        Object json = root.get("opinionExtendida");
+        if (json instanceof JSONObject) {
+            JSONObject opinionExtendida = root.getJSONObject("opinionExtendida");
+            JSONObject excursion = opinionExtendida.getJSONObject("excursion");
+            JSONObject usuario = opinionExtendida.getJSONObject("usuario");
 
-            opinionExtendida = opinionesExtendidas.getJSONObject(i);
-            excursion = opinionExtendida.getJSONObject("excursion");
-            usuario = opinionExtendida.getJSONObject("usuario");
+            OpinionExtendida oe = new OpinionExtendida();
 
             oe.setIdOpinion(opinionExtendida.getInt("idOpinion"));
             oe.setUsuario(new UsuarioDB(usuario.getString("idUsuario")));
@@ -71,23 +68,49 @@ public class RestJSONParserManager {
             oe.setImgPath(opinionExtendida.getString("imgPath"));
 
             loe.add(oe);
-        }
 
-        return loe;
+            return loe;
+        } else if (json instanceof JSONArray) {
+            JSONArray opinionesExtendidas = root.getJSONArray("opinionExtendida");
+
+            OpinionExtendida oe;
+            JSONObject opinionExtendida;
+            JSONObject excursion;
+            JSONObject usuario;
+            for (int i=0; i < opinionesExtendidas.length(); i++) {
+                oe = new OpinionExtendida();
+
+                opinionExtendida = opinionesExtendidas.getJSONObject(i);
+                excursion = opinionExtendida.getJSONObject("excursion");
+                usuario = opinionExtendida.getJSONObject("usuario");
+
+                oe.setIdOpinion(opinionExtendida.getInt("idOpinion"));
+                oe.setUsuario(new UsuarioDB(usuario.getString("idUsuario")));
+                oe.setExcursion(new ExcursionDB(excursion.getInt("idExcursion"), excursion.getString("nombre"), excursion.getString("nivel"),
+                        excursion.getString("lugar"), excursion.getDouble("distancia"), excursion.getString("foto"),
+                        Float.parseFloat(excursion.getString("latitud")), Float.parseFloat(excursion.getString("longitud"))));
+                oe.setOpinion(opinionExtendida.getString("opinion"));
+                oe.setImgPath(opinionExtendida.getString("imgPath"));
+
+                loe.add(oe);
+            }
+
+            return loe;
+        } else {
+            return loe;
+        }
     }
 
     public static ArrayList<ExcursionDestacada> getExcursionesDestacadas(String data) throws JSONException {
         ArrayList<ExcursionDestacada> aled = new ArrayList<ExcursionDestacada>();
 
         JSONObject root = new JSONObject(data);
-        JSONArray excursionesDestacadas = root.getJSONArray("excursionDestacada");
 
-        ExcursionDestacada ed;
-        JSONObject excursionDestacada;
-        for (int i=0; i < excursionesDestacadas.length(); i++) {
-            ed = new ExcursionDestacada();
+        Object json = root.get("excursionDestacada");
+        if (json instanceof JSONObject) {
+            JSONObject excursionDestacada = root.getJSONObject("excursionDestacada");
 
-            excursionDestacada = excursionesDestacadas.getJSONObject(i);
+            ExcursionDestacada ed = new ExcursionDestacada();
 
             ed.setIdExcursion(excursionDestacada.getInt("idExcursion"));
             ed.setNombre(excursionDestacada.getString("nombre"));
@@ -100,23 +123,47 @@ public class RestJSONParserManager {
             ed.setNumOpiniones(excursionDestacada.getInt("numOpiniones"));
 
             aled.add(ed);
-        }
 
-        return aled;
+            return aled;
+        } else if (json instanceof JSONArray) {
+            JSONArray excursionesDestacadas = root.getJSONArray("excursionDestacada");
+
+            ExcursionDestacada ed;
+            JSONObject excursionDestacada;
+            for (int i=0; i < excursionesDestacadas.length(); i++) {
+                ed = new ExcursionDestacada();
+
+                excursionDestacada = excursionesDestacadas.getJSONObject(i);
+
+                ed.setIdExcursion(excursionDestacada.getInt("idExcursion"));
+                ed.setNombre(excursionDestacada.getString("nombre"));
+                ed.setNivel(excursionDestacada.getString("nivel"));
+                ed.setLugar(excursionDestacada.getString("lugar"));
+                ed.setDistancia(excursionDestacada.getDouble("distancia"));
+                ed.setFoto(excursionDestacada.getString("foto"));
+                ed.setLatitud(Float.parseFloat(excursionDestacada.getString("latitud")));
+                ed.setLongitud(Float.parseFloat(excursionDestacada.getString("longitud")));
+                ed.setNumOpiniones(excursionDestacada.getInt("numOpiniones"));
+
+                aled.add(ed);
+            }
+
+            return aled;
+        } else {
+            return aled;
+        }
     }
 
     public static ArrayList<ExcursionDB> getExcursionesBusqueda(String data) throws JSONException {
         ArrayList<ExcursionDB> ale = new ArrayList<ExcursionDB>();
 
         JSONObject root = new JSONObject(data);
-        JSONArray excursiones = root.getJSONArray("excursion");
 
-        ExcursionDB e;
-        JSONObject excursion;
-        for (int i = 0; i < excursiones.length(); i++) {
-            e = new ExcursionDB();
+        Object json = root.get("excursion");
+        if (json instanceof JSONObject) {
+            JSONObject excursion = root.getJSONObject("excursion");
 
-            excursion = excursiones.getJSONObject(i);
+            ExcursionDB e = new ExcursionDB();
 
             e.setIdExcursion(excursion.getInt("idExcursion"));
             e.setNombre(excursion.getString("nombre"));
@@ -128,24 +175,46 @@ public class RestJSONParserManager {
             e.setLongitud(Float.parseFloat(excursion.getString("longitud")));
 
             ale.add(e);
-        }
 
-        return ale;
+            return ale;
+        } else if (json instanceof JSONArray) {
+            JSONArray excursiones = root.getJSONArray("excursion");
+
+            ExcursionDB e;
+            JSONObject excursion;
+            for (int i = 0; i < excursiones.length(); i++) {
+                e = new ExcursionDB();
+
+                excursion = excursiones.getJSONObject(i);
+
+                e.setIdExcursion(excursion.getInt("idExcursion"));
+                e.setNombre(excursion.getString("nombre"));
+                e.setNivel(excursion.getString("nivel"));
+                e.setLugar(excursion.getString("lugar"));
+                e.setDistancia(excursion.getDouble("distancia"));
+                e.setFoto(excursion.getString("foto"));
+                e.setLatitud(Float.parseFloat(excursion.getString("latitud")));
+                e.setLongitud(Float.parseFloat(excursion.getString("longitud")));
+
+                ale.add(e);
+            }
+
+            return ale;
+        } else {
+            return ale;
+        }
     }
 
     public static ArrayList<OpinionDB> getOpinionesPorIdExcursion(String data) throws JSONException {
         ArrayList<OpinionDB> alo = new ArrayList<OpinionDB>();
 
         JSONObject root = new JSONObject(data);
-        JSONArray opiniones = root.getJSONArray("opinion");
 
-        OpinionDB o;
-        JSONObject opinion;
-        for (int i = 0; i < opiniones.length(); i++) {
-            o = new OpinionDB();
+        Object json = root.get("opinion");
+        if (json instanceof JSONObject) {
+            JSONObject opinion = root.getJSONObject("opinion");
 
-            opinion = opiniones.getJSONObject(i);
-
+            OpinionDB o = new OpinionDB();
             o.setIdOpinion(opinion.getInt("idOpinion"));
             o.setIdUsuario(opinion.getString("idUsuario"));
             o.setIdExcursion(opinion.getInt("idExcursion"));
@@ -153,8 +222,30 @@ public class RestJSONParserManager {
             o.setFoto(opinion.getString("foto"));
 
             alo.add(o);
-        }
 
-        return alo;
+            return alo;
+        } else if (json instanceof JSONArray) {
+            JSONArray opiniones = root.getJSONArray("opinion");
+
+            OpinionDB o;
+            JSONObject opinion;
+            for (int i = 0; i < opiniones.length(); i++) {
+                o = new OpinionDB();
+
+                opinion = opiniones.getJSONObject(i);
+
+                o.setIdOpinion(opinion.getInt("idOpinion"));
+                o.setIdUsuario(opinion.getString("idUsuario"));
+                o.setIdExcursion(opinion.getInt("idExcursion"));
+                o.setOpinion(opinion.getString("opinion"));
+                o.setFoto(opinion.getString("foto"));
+
+                alo.add(o);
+            }
+
+            return alo;
+        } else {
+            return alo;
+        }
     }
 }
